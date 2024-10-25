e5a3eac1561e5fc3155cbb66280fca47deb357f2
João Rafael Sanches <jsanchesedantas@gmail.com>
2024-10-25 20:51:48 -0300

    material workshop

---
+++ workshop python/primeiro.py
@@ -0,0 +1,14 @@
+print("meu primeiro teste em python")
+
+if 5 > 2:
+    print("cinco e maior que 2!")
+
+    #variavel
+x = "incrivel"
+
+#função
+def syfunc():
+    print("python  " + x)
+
+#chamada a função
+syfunc()
